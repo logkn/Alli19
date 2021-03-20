e8a1537b7aae1b4218cb941ff37a614bd3e5e26e
logkn <logan@vettd.ai>
2021-03-20 00:11:32 -0700

    s

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{
+      textAlign: 'left',
+      padding: 100
+    }}>
+      <p>
+      Alli,
+      </p>
+      <p>
+      Words cannot begin to describe how thankful I am for your conception 19 years and 9 months ago.
+      </p>
+      <p>
+      But numbers can, 12/10.
+      </p>
+      <p>
+      In all seriousness though, March 20 means the world to me now. Aries szn, springtime, and the love of my life’s big day. Just in case you ever wanna go back and read this, for context, you’re with some kappas partaying in Leavenworth rn and I’m back at “home” feeling the distance. Yes, quotes on the home—even though we aren’t very far, home to me at this point is seriously wherever you are. And with that, I so wish I could be home right now, celebrating your big day with you. But that’s ok, I guess, because if the world doesn’t come crashing down around us, I’ll be able to celebrate many, many more birthdays with you. 
+      </p>
+      <p>
+      Honestly as I’m writing this, I’m realizing how much you’ve changed my outlook on life. Before I met you, birthdays were absolutely huge to me, hence the long ass birthday messages. But you’ve taught me to enjoy everyday, regardless of labels. Obviously, March 20, 2002 has much more associated to it than just a label, and of fucking course we’re going to celebrate it accordingly, however we can over the distance. But more than anything, I want to celebrate every day with you like it’s your birthday.
+      </p>
+      <p>
+      When I think about you, I recognize that I’ve been so crazy happy with you over the last couple months, like it’s seriously too good to be true. As an introvert who’d much rather pick and choose a few people in his life to build extraordinarily close relationships with, there are definitely a few types of people I want around me in my life—I want a significant other, a best friend, a gaming buddy, and a food adventure mate, just to name a few. But meeting you has shown me, they don’t need to be separate people. Somehow, you are everything I want in a person who I want to spend time with. I wouldn’t change a thing about you, and that makes me question reality a little.
+      </p>
+      <p>
+      But then I realize—the only explanation is that I manifested you. You’re too perfect for me to be an actual human, so I seriously must have just wished your existence at some point. One of these past 19 years, on some particular March 20th, I must have seen a shooting star or looked at the clock right when it said 11:11 or something, but your existence was definitely my doing. Only reasonable explanation…
+      </p>
+      <p>
+      … no, you’re real. After some very particular series of events, I met a girl who was born 18 years, 6 months, and 13 days ago. My flirty ass caught onto her pride of how the stars aligned that day 18 and a half months ago, and called her an asparagus one too many times. But really, I called her an asparagus just enough times to get her number and realize that girl was one I’d never want to be apart from.
+      </p>
+      <p>
+      Happy 19th Alli. I love you most. ❤️
+      </p>
+    </div>
+  );
+}
+
+export default App;
